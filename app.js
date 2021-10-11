9171a6aa67aef78e42100483bc54f074da772c58
GaryFinance <77881011+GaryFinance@users.noreply.github.com>
2021-10-11 21:33:03 +0900

    ejs 문법

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 var express = require("express")
-
+var path = require('path')
 var app = express()
 
+app.set('views',path.resolve(__dirname ,'views'))
+// console.log(path.resolve(__dirname ,'views'))
+app.set('view engine', 'ejs')
 
-app.get('', function(req, res){
-    res.send("Hello World!!")
+app.engine('html', require('ejs').renderFile)
+
+app.get('/', function(req, res){
+    return res.render('index.html',{name:"태경"})
 })
 
+
 app.get("/data",function(req, res){
-    res.send("데이터 보낸겁니다.")
+    return res.send("데이터 보낸겁니다.")
 })
 
 app.listen(8080, function(){
